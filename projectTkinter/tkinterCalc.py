58ea358a811dff2f9ce314ee893a153de2a4ecd4
PaulDVS <91812462+PaulDVS@users.noreply.github.com>
2023-03-24 15:00:43 +0000

    Add files via upload
    Add square root, fixes regarding backspace function, improved look

--- projectTkinter/tkinterCalc.py
+++ projectTkinter/tkinterCalc.py
@@ -1,5 +1,6 @@
 # Paul van Sittert 24.3.23
 from tkinter import *
+from math import sqrt
 
 # Set up var
 calculatorString = ""
@@ -16,10 +17,10 @@ def addString(string):
     outputString()
 
 def openBracket():
-    global calculatorString
     global bracket
     addString("()")
     bracket +=1
+    outputString()
 
 def closeBracket():
     global bracket
@@ -27,125 +28,159 @@ def closeBracket():
     if bracket >0:
         bracket -=1
 
+    outputString()
+
+def openSqrt():
+    addString("sqrt")
+    openBracket()
+
 def backSpace():
     global calculatorString
-    calculatorString = calculatorString[:-1]
+    global bracket
+
+    if(len(calculatorString)>0):
+        if (len(calculatorString) > 1 and calculatorString[len(calculatorString) - bracket - 2] == "t"):
+            newString = calculatorString[:len(calculatorString) - bracket - 5] + calculatorString[len(calculatorString) - bracket+1:]
+            calculatorString = newString
+            bracket -= 1
+        elif(calculatorString[len(calculatorString)-bracket-1] == "("):
+            newString = calculatorString[:len(calculatorString) - bracket-1] + calculatorString[len(calculatorString) - bracket +1:]
+            calculatorString = newString
+            bracket -= 1
+        else:
+            newString = calculatorString[:len(calculatorString) - bracket - 1] + calculatorString[len(calculatorString) - bracket:]
+            calculatorString = newString
+
     outputString()
 
 def clearString():
     global calculatorString
+    global answerNum
+    global bracket
+
     calculatorString = ""
     answerNum = 0
+    bracket = 0
     outputString()
 
 
 def calcInput():
     global calculatorString
     global answerNum
+    global bracket
+
     try:
         answerNum = eval(calculatorString)
         calculatorString = calculatorString + " = " + str(answerNum)
-        outputString()
     except:
         calculatorString = "Invalid Calculation"
-        outputString()
 
+    bracket = 0
+    outputString()
     calculatorString = ""
 
 
 def outputString():
-    output_Label.config(text=calculatorString)
+    newString = calculatorString[:len(calculatorString)-bracket] + " >" + calculatorString[len(calculatorString)-bracket:]
+    output_Label.config(text=newString)
 
 
 # Set up gui
 root = Tk()
 root.title("My calculator")
-root.config(bg="grey")
+root.config(bg="#1E20CB")
+root.resizable(False, False)
 
 # Set up frames
 output_frame = Frame(root, width=100, height=50)
 output_frame.grid(row=0, column=0, padx=10, pady=10)
 
-output_Label = Label(output_frame, text="", width=100, anchor="e", justify=RIGHT, padx=10)
+output_Label = Label(output_frame, bg="#6EB3F7", text="", width=60, font=("Times",20), anchor="e", justify=RIGHT, padx=10)
 output_Label.pack()
 
-button_Menu = Frame(root, width=100, height=400)
+button_Menu = Frame(root, width=100, height=400, bg="#1E76CB")
 button_Menu.grid(row=1, column=0, padx=10, pady=10)
 
 # Set up buttons
-filler1_lable = Label(button_Menu, width=10)
+filler1_lable = Label(button_Menu, width=30, bg="#1E76CB")
 filler1_lable.grid(row=0, column=0)
 
-filler2_lable = Label(button_Menu, width=10)
-filler2_lable.grid(row=0, column=6)
+filler2_lable = Label(button_Menu, width=30, bg="#1E76CB")
+filler2_lable.grid(row=7, column=6)
 
-button1 = Button(button_Menu, text="   1   ", command=lambda arg1="1": addString(arg1))
-button1.grid(row=3, column=1, padx=10, pady=10)
+#Number Buttons
+button1 = Button(button_Menu, bg="#6EB3F7", text="   1   ", font=("Times",18), command=lambda arg1="1": addString(arg1))
+button1.grid(row=4, column=1, padx=10, pady=10)
 
-button2 = Button(button_Menu, text="   2   ", command=lambda arg1="2": addString(arg1))
-button2.grid(row=3, column=2, padx=10, pady=10)
+button2 = Button(button_Menu, bg="#6EB3F7", text="   2   ", font=("Times",18), command=lambda arg1="2": addString(arg1))
+button2.grid(row=4, column=2, padx=10, pady=10)
 
-button3 = Button(button_Menu, text="   3   ", command=lambda arg1="3": addString(arg1))
-button3.grid(row=3, column=3, padx=10, pady=10)
+button3 = Button(button_Menu, bg="#6EB3F7", text="   3   ", font=("Times",18), command=lambda arg1="3": addString(arg1))
+button3.grid(row=4, column=3, padx=10, pady=10)
 
-button4 = Button(button_Menu, text="   4   ", command=lambda arg1="4": addString(arg1))
-button4.grid(row=2, column=1, padx=10, pady=10)
+button4 = Button(button_Menu, bg="#6EB3F7", text="   4   ", font=("Times",18), command=lambda arg1="4": addString(arg1))
+button4.grid(row=3, column=1, padx=10, pady=10)
 
-button5 = Button(button_Menu, text="   5   ", command=lambda arg1="5": addString(arg1))
-button5.grid(row=2, column=2, padx=10, pady=10)
+button5 = Button(button_Menu, bg="#6EB3F7", text="   5   ", font=("Times",18), command=lambda arg1="5": addString(arg1))
+button5.grid(row=3, column=2, padx=10, pady=10)
 
-button6 = Button(button_Menu, text="   6   ", command=lambda arg1="6": addString(arg1))
-button6.grid(row=2, column=3, padx=10, pady=10)
+button6 = Button(button_Menu, bg="#6EB3F7", text="   6   ", font=("Times",18), command=lambda arg1="6": addString(arg1))
+button6.grid(row=3, column=3, padx=10, pady=10)
 
-button7 = Button(button_Menu, text="   7   ", command=lambda arg1="7": addString(arg1))
-button7.grid(row=1, column=1, padx=10, pady=10)
+button7 = Button(button_Menu, bg="#6EB3F7", text="   7   ", font=("Times",18), command=lambda arg1="7": addString(arg1))
+button7.grid(row=2, column=1, padx=10, pady=10)
 
-button8 = Button(button_Menu, text="   8   ", command=lambda arg1="8": addString(arg1))
-button8.grid(row=1, column=2, padx=10, pady=10)
+button8 = Button(button_Menu, bg="#6EB3F7", text="   8   ", font=("Times",18), command=lambda arg1="8": addString(arg1))
+button8.grid(row=2, column=2, padx=10, pady=10)
 
-button9 = Button(button_Menu, text="   9   ", command=lambda arg1="9": addString(arg1))
-button9.grid(row=1, column=3, padx=10, pady=10)
+button9 = Button(button_Menu, bg="#6EB3F7", text="   9   ", font=("Times",18), command=lambda arg1="9": addString(arg1))
+button9.grid(row=2, column=3, padx=10, pady=10)
 
-button0 = Button(button_Menu, text="   0   ", command=lambda arg1="0": addString(arg1))
-button0.grid(row=4, column=1, padx=10, pady=10)
+button0 = Button(button_Menu, bg="#6EB3F7", text="           0           ", font=("Times",18), command=lambda arg1="0": addString(arg1))
+button0.grid(row=5, column=1, columnspan=2, padx=10, pady=10)
 
-buttonDecimal = Button(button_Menu, text="   .   ", command=lambda arg1=".": addString(arg1))
-buttonDecimal.grid(row=4, column=3, padx=10, pady=10)
+#Operator buttons
+buttonDecimal = Button(button_Menu, bg="#6EB3F7", text="   .   ", font=("Times",18), command=lambda arg1=".": addString(arg1))
+buttonDecimal.grid(row=5, column=3, padx=10, pady=10)
 
-buttonPlus = Button(button_Menu, text="   +   ", command=lambda arg1="+": addString(arg1))
-buttonPlus.grid(row=4, column=4, padx=10, pady=10)
+buttonPlus = Button(button_Menu, bg="#6EB3F7", text="   +   ", font=("Times",18), command=lambda arg1="+": addString(arg1))
+buttonPlus.grid(row=5, column=4, padx=10, pady=10)
 
-buttonMinus = Button(button_Menu, text="   -   ", command=lambda arg1="-": addString(arg1))
-buttonMinus.grid(row=3, column=4, padx=10, pady=10)
+buttonMinus = Button(button_Menu, bg="#6EB3F7", text="   -   ", font=("Times",18), command=lambda arg1="-": addString(arg1))
+buttonMinus.grid(row=4, column=4, padx=10, pady=10)
 
-buttonMult = Button(button_Menu, text="   *   ", command=lambda arg1="*": addString(arg1))
-buttonMult.grid(row=2, column=4, padx=10, pady=10)
+buttonMult = Button(button_Menu, bg="#6EB3F7", text="   *   ", font=("Times",18), command=lambda arg1="*": addString(arg1))
+buttonMult.grid(row=3, column=4, padx=10, pady=10)
 
-buttonDiv = Button(button_Menu, text="   /   ", command=lambda arg1="/": addString(arg1))
-buttonDiv.grid(row=1, column=4, padx=10, pady=10)
+buttonDiv = Button(button_Menu, bg="#6EB3F7", text="   /   ", font=("Times",18), command=lambda arg1="/": addString(arg1))
+buttonDiv.grid(row=2, column=4, padx=10, pady=10)
 
-buttonDiv = Button(button_Menu, text="   %   ", command=lambda arg1="%": addString(arg1))
-buttonDiv.grid(row=1, column=5, padx=10, pady=10)
+buttonMod = Button(button_Menu, bg="#6EB3F7", text="   %   ", font=("Times",18), command=lambda arg1="%": addString(arg1))
+buttonMod.grid(row=2, column=5, padx=10, pady=10)
 
-buttonDiv = Button(button_Menu, text="   ^   ", command=lambda arg1="**": addString(arg1))
-buttonDiv.grid(row=2, column=5, padx=10, pady=10)
+buttonPow = Button(button_Menu, bg="#6EB3F7", text="   ^   ", font=("Times",18), command=lambda arg1="**": addString(arg1))
+buttonPow.grid(row=3, column=5, padx=10, pady=10)
 
-buttonAnswr = Button(button_Menu, text=" Ans ", command=lambda: addString(str(answerNum)))
-buttonAnswr.grid(row=0, column=1, padx=10, pady=10)
+buttonSqrt = Button(button_Menu, bg="#6EB3F7", text="   √   ", font=("Times",18), command=lambda : openSqrt())
+buttonSqrt.grid(row=4, column=5, padx=10, pady=10)
 
-buttonOpenBrack = Button(button_Menu, text="   (   ", command=lambda: openBracket())
-buttonOpenBrack.grid(row=0, column=2, padx=10, pady=10)
+buttonAnswr = Button(button_Menu, bg="#6EB3F7", text=" Ans ", font=("Times",18), command=lambda: addString(str(answerNum)))
+buttonAnswr.grid(row=1, column=1, padx=10, pady=10)
 
-buttonCloseBrack = Button(button_Menu, text="   )   ", command=lambda: closeBracket())
-buttonCloseBrack.grid(row=0, column=3, padx=10, pady=10)
+buttonOpenBrack = Button(button_Menu, bg="#6EB3F7", text="   (   ", font=("Times",18), command=lambda: openBracket())
+buttonOpenBrack.grid(row=1, column=2, padx=10, pady=10)
 
-buttonBack = Button(button_Menu, text=" <== ", command=lambda: backSpace())
-buttonBack.grid(row=0, column=4, padx=10, pady=10)
+buttonCloseBrack = Button(button_Menu, bg="#6EB3F7", text="   )   ", font=("Times",18), command=lambda: closeBracket())
+buttonCloseBrack.grid(row=1, column=3, padx=10, pady=10)
 
-buttonClr = Button(button_Menu, text="   C   ", command=lambda: clearString())
-buttonClr.grid(row=0, column=5, padx=10, pady=10)
+buttonBack = Button(button_Menu, bg="#6EB3F7", text=" <== ", font=("Times",18), command=lambda: backSpace())
+buttonBack.grid(row=1, column=4, padx=10, pady=10)
 
-buttonCalc = Button(button_Menu, text="   =   ", command=lambda: calcInput())
-buttonCalc.grid(row=4, column=5, padx=10, pady=10)
+buttonClr = Button(button_Menu, bg="#6EB3F7", text="   C   ", font=("Times",18), command=lambda: clearString())
+buttonClr.grid(row=1, column=5, padx=10, pady=10)
 
+buttonCalc = Button(button_Menu, bg="#6EB3F7", text="   =   ", font=("Times",18), command=lambda: calcInput())
+buttonCalc.grid(row=5, column=5, padx=10, pady=10)
+
+outputString()
 root.mainloop()
